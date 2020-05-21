b9e6dd172c8a1e9c7bf80cae43342c3e7fa57798
NotLebedev <artem-itf@yandex.ru>
2020-05-21 17:38:29 +0300

    *Fix of ExecutionContext.toBytecodes

--- src/main/java/org/notlebedev/ExecutionContext.java
+++ src/main/java/org/notlebedev/ExecutionContext.java
@@ -1,30 +1,47 @@
 package org.notlebedev;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.instrument.Instrumentation;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SuppressWarnings("rawtypes")
 public class ExecutionContext {
     private final Set<Class> loadedClasses;
+
+    /**
+     * Default java packages that are the same in all ctx
+     */
+    private final static String[] packageBlackList = {
+            "java",
+            "sun",
+            "javax",
+            "com",
+            "jdk",
+            "org"
+    };
 
     public ExecutionContext(Instrumentation instrumentation) {
         loadedClasses = new HashSet<>(instrumentation.getAllLoadedClasses().length);
         loadedClasses.addAll(Arrays.asList(instrumentation.getAllLoadedClasses()));
         loadedClasses.removeIf(Class::isArray);
         loadedClasses.removeIf(Class::isPrimitive);
+        Arrays.stream(packageBlackList)
+                .forEach(pckg -> loadedClasses.removeIf(clazz -> clazz.getTypeName().startsWith(pckg)));
     }
 
     /**
-     * @return {@link Set} of class names from this execution context, as retrieved by {@code Class::getName} function
+     * @return {@link Set} of class names from this execution context, as retrieved by {@code Class::getName} function,
+     * excluding default java library classes
      */
-    public Set<String> getLoadedClassNames() {
+    public Set<String> getExtraLoadedClassNames() {
         return loadedClasses.stream().map(Class::getName).collect(Collectors.toCollection(HashSet::new));
     }
 
     /**
-     * Get all classes from execution context that are not present in list
+     * Get all classes from execution context (except default java library) that are not present in list
      * @param presentClassNames {@link List} of full class names
      * @return {@link Set} of classes
      */
@@ -37,8 +54,9 @@ public class ExecutionContext {
     public static Map<String, byte[]> toBytecodes(Set<Class> classes) throws IOException {
         var result = new HashMap<String, byte[]>(classes.size());
         for (Class clazz : classes) {
-            byte[] bytecode = clazz.getResource(clazz.getSimpleName() + ".class").openStream().readAllBytes();
-            result.put(clazz.getName(), bytecode);
+            InputStream byteIn = clazz.getResourceAsStream(
+                    clazz.getTypeName().replace(clazz.getPackageName() + ".", "") + ".class");
+            result.put(clazz.getName(), byteIn.readAllBytes());
         }
         return result;
     }
