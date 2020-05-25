84d455b5cfb77fd27f4d19bd0c832131f05315f0
NotLebedev <artem-itf@yandex.ru>
2020-05-25 17:48:29 +0300

    +Recursive introspection *Changed project sdk to jdk11

--- src/main/java/org/notlebedev/ClassIntrospection.java
+++ src/main/java/org/notlebedev/ClassIntrospection.java
@@ -7,26 +7,21 @@ import org.objectweb.asm.Opcodes;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.*;
-import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class ClassIntrospection extends ClassVisitor {
     private final Class<?> clazz;
     private final Set<Class<?>> usedClasses;
-    private final Set<Class<?>> classesInspected;
+    private final Set<Class<?>> classesKnown;
     private final List<Exception> exceptions;
 
     public ClassIntrospection(Class<?> clazz, Set<Class<?>> omitClasses) throws SyntheticClassException {
         super(Opcodes.ASM7);
         usedClasses = new HashSet<>();
         exceptions = new ArrayList<>();
-        classesInspected = new HashSet<>(omitClasses);
+        classesKnown = new HashSet<>(omitClasses);
         this.clazz = clazz;
         if (clazz.isSynthetic())
             throw new SyntheticClassException();
@@ -37,7 +32,7 @@ public class ClassIntrospection extends ClassVisitor {
                 clazz.getTypeName().replace(clazz.getPackageName() + ".", "") + ".class");
         var cr = new ClassReader(byteIn);
         cr.accept(this, 0);
-        return usedClasses;
+        return classesKnown;
     }
 
     @Override
@@ -116,43 +111,18 @@ public class ClassIntrospection extends ClassVisitor {
 
     @Override
     public void visitEnd() {
-        super.visitEnd();
-    }
-
-    public static Set<Class<?>> getUsedClasses(Class<?> clazz, Set<Class<?>> classesKnown) {
-        Set<Class<?>> usedClasses = new HashSet<>();
-        usedClasses.addAll(Arrays.stream(clazz.getDeclaredFields()).map(Field::getType)
-                .collect(Collectors.toCollection(ArrayList::new)));
-        usedClasses.addAll(Arrays.stream(clazz.getDeclaredClasses())
-                .collect(Collectors.toCollection(ArrayList::new)));
-
-        usedClasses.addAll(Arrays.stream(clazz.getDeclaredMethods()).map(Method::getReturnType)
-                .collect(Collectors.toCollection(ArrayList::new)));
-        usedClasses.addAll(Arrays.stream(clazz.getDeclaredMethods()).map(Method::getParameterTypes).flatMap(Arrays::stream)
-                .collect(Collectors.toCollection(ArrayList::new)));
-        usedClasses.addAll(Arrays.stream(clazz.getDeclaredMethods()).map(Method::getExceptionTypes).flatMap(Arrays::stream)
-                .collect(Collectors.toCollection(ArrayList::new)));
-
-        usedClasses.addAll(Arrays.stream(clazz.getConstructors()).map(Constructor::getParameterTypes)
-                .flatMap(Arrays::stream)
-                .collect(Collectors.toCollection(ArrayList::new)));
-        usedClasses.addAll(Arrays.stream(clazz.getConstructors()).map(Constructor::getExceptionTypes)
-                .flatMap(Arrays::stream)
-                .collect(Collectors.toCollection(ArrayList::new)));
-
-        if(clazz.getComponentType() != null)
-            usedClasses.add(clazz.getComponentType()); //If class is array add its component type
-
         usedClasses.removeAll(classesKnown);
         classesKnown.addAll(usedClasses);
-
         Set<Class<?>> recursiveUsed = new HashSet<>();
-        usedClasses.forEach(aClass -> recursiveUsed.addAll(getUsedClasses (aClass, classesKnown)));
+        usedClasses.forEach(aClass -> {
+            try {
+                var introspection = new ClassIntrospection(aClass, classesKnown);
+                recursiveUsed.addAll(introspection.getUsedClasses());
+            } catch (SyntheticClassException | IOException ignored) {
+            }
+        });
 
         classesKnown.addAll(recursiveUsed);
-
-        classesKnown.removeIf(Class::isArray);
-        return classesKnown;
+        super.visitEnd();
     }
-
 }
