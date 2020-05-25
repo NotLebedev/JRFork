b374e4b2109d266209297caf03ef829328d242f4
NotLebedev <artem-itf@yandex.ru>
2020-05-25 14:02:33 +0300

    *Changed ClassIntrospection to be an ASM visitor to introspect code of methods +ASM library dependency

--- src/main/java/org/notlebedev/ClassIntrospection.java
+++ src/main/java/org/notlebedev/ClassIntrospection.java
@@ -1,12 +1,77 @@
 package org.notlebedev;
 
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Opcodes;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class ClassIntrospection {
+public class ClassIntrospection extends ClassVisitor {
+    private final Set<Class<?>> usedClasses;
+    private final Set<Class<?>> classesInspected;
+
+    public ClassIntrospection() {
+        super(Opcodes.ASM7);
+        usedClasses = new HashSet<>();
+        classesInspected = new HashSet<>();
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
+        try {
+            System.out.println(forName(descriptor));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(signature);
+        return super.visitField(access, name, descriptor, signature, value);
+    }
+
+    private static Class<?> forName(String name) throws ClassNotFoundException {
+        name = name.replace(" ", "");
+
+        switch (name) {
+
+            case "boolean":
+            case "Z":
+                return boolean.class;
+            case "byte":
+            case "B":
+                return byte.class;
+            case "char":
+            case "C":
+                return char.class;
+            case "long":
+            case "J":
+                return long.class;
+            case "short":
+            case "S":
+                return short.class;
+            case "int":
+            case "I":
+                return int.class;
+            case "float":
+            case "F":
+                return float.class;
+            case "double":
+            case "D":
+                return double.class;
+            case "void":
+                return void.class;
+            default:
+        }
+
+        while (name.startsWith("["))
+            name = name.substring(1);
+        name = name.substring(1);
+        name = name.replace(";", "");
+        name = name.replace("/", ".");
+        return Class.forName(name);
+    }
 
     public static Set<Class<?>> getUsedClasses(Class<?> clazz, Set<Class<?>> classesKnown) {
         Set<Class<?>> usedClasses = new HashSet<>();
