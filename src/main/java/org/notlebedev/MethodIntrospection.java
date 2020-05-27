8656741d534e5346fa77360d31af8883a52905a8
NotLebedev <artem-itf@yandex.ru>
2020-05-27 16:50:49 +0300

    +Implemented MethodIntrospection

--- src/main/java/org/notlebedev/MethodIntrospection.java
+++ src/main/java/org/notlebedev/MethodIntrospection.java
@@ -1,15 +1,118 @@
 package org.notlebedev;
 
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class MethodIntrospection extends MethodVisitor {
+    private final Set<Class<?>> usedClasses;
+    private final List<ClassNotFoundException> exceptions;
     public MethodIntrospection() {
         super(Opcodes.ASM7);
+        usedClasses = new HashSet<>();
+        exceptions = new ArrayList<>();
     }
 
     @Override
-    public void visitInsn(int opcode) {
-        super.visitInsn(opcode);
+    public void visitTypeInsn(int opcode, String type) {
+        ldc(type);
+        super.visitTypeInsn(opcode, type);
+    }
+
+    @Override
+    public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
+        ldc(descriptor);
+        super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+    }
+
+    @Override
+    public void visitInvokeDynamicInsn(String name, String descriptor, Handle bootstrapMethodHandle, Object... bootstrapMethodArguments) {
+        ldc(descriptor, bootstrapMethodHandle.getDesc());
+        super.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
+    }
+
+    @Override
+    public void visitTryCatchBlock(Label start, Label end, Label handler, String type) {
+        ldc(type);
+        super.visitTryCatchBlock(start, end, handler, type);
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
+        ldc(descriptor);
+        return super.visitAnnotation(descriptor, visible);
+    }
+
+    @Override
+    public AnnotationVisitor visitTypeAnnotation(int typeRef, TypePath typePath, String descriptor, boolean visible) {
+        ldc(descriptor);
+        return super.visitTypeAnnotation(typeRef, typePath, descriptor, visible);
+    }
+
+    @Override
+    public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
+        ldc(descriptor);
+        super.visitFieldInsn(opcode, owner, name, descriptor);
+    }
+
+    @Override
+    public AnnotationVisitor visitParameterAnnotation(int parameter, String descriptor, boolean visible) {
+        ldc(descriptor);
+        return super.visitParameterAnnotation(parameter, descriptor, visible);
+    }
+
+    @Override
+    public void visitMultiANewArrayInsn(String descriptor, int numDimensions) {
+        ldc(descriptor);
+        super.visitMultiANewArrayInsn(descriptor, numDimensions);
+    }
+
+    @Override
+    public AnnotationVisitor visitInsnAnnotation(int typeRef, TypePath typePath, String descriptor, boolean visible) {
+        ldc(descriptor);
+        return super.visitInsnAnnotation(typeRef, typePath, descriptor, visible);
+    }
+
+    @Override
+    public AnnotationVisitor visitTryCatchAnnotation(int typeRef, TypePath typePath, String descriptor, boolean visible) {
+        ldc(descriptor);
+        return super.visitTryCatchAnnotation(typeRef, typePath, descriptor, visible);
+    }
+
+    @Override
+    public void visitLocalVariable(String name, String descriptor, String signature, Label start, Label end, int index) {
+        ldc(descriptor, signature);
+        super.visitLocalVariable(name, descriptor, signature, start, end, index);
+    }
+
+    @Override
+    public AnnotationVisitor visitLocalVariableAnnotation(int typeRef, TypePath typePath, Label[] start, Label[] end, int[] index, String descriptor, boolean visible) {
+        ldc(descriptor);
+        return super.visitLocalVariableAnnotation(typeRef, typePath, start, end, index, descriptor, visible);
+    }
+
+    /**
+     * Load classes specified in descriptor in {@code usedClasses} {@link Set}
+     * @param descriptors descriptors with classes in format Lclass/path/name
+     */
+    private void ldc(String... descriptors) {
+        for (String descriptor : descriptors) {
+            try {
+                usedClasses.addAll(ClassIntrospection.allClassNames(descriptor));
+            } catch (ClassNotFoundException e) {
+                exceptions.add(e);
+            }
+        }
+    }
+
+    public Set<Class<?>> getUsedClasses() {
+        return usedClasses;
+    }
+
+    public List<ClassNotFoundException> getExceptions() {
+        return exceptions;
     }
 }
