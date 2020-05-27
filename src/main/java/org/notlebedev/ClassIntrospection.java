964a9c649061ed8d9afc56be76f819e2b1174698
NotLebedev <artem-itf@yandex.ru>
2020-05-27 16:22:33 +0300

    *Changed order of exception usage +Method introspection wired in logic

--- src/main/java/org/notlebedev/ClassIntrospection.java
+++ src/main/java/org/notlebedev/ClassIntrospection.java
@@ -12,7 +12,8 @@ public class ClassIntrospection extends ClassVisitor {
     private final Class<?> clazz;
     private final Set<Class<?>> usedClasses;
     private final Set<Class<?>> classesKnown;
-    private final List<Exception> exceptions;
+    private final List<ClassNotFoundException> exceptions;
+    private final MethodIntrospection methodIntrospection;
 
     public ClassIntrospection(Class<?> clazz, Set<Class<?>> omitClasses) throws SyntheticClassException {
         super(Opcodes.ASM7);
@@ -22,13 +23,16 @@ public class ClassIntrospection extends ClassVisitor {
         this.clazz = clazz;
         if (clazz.isSynthetic())
             throw new SyntheticClassException();
+        methodIntrospection = new MethodIntrospection();
     }
 
-    public Set<Class<?>> getUsedClasses() throws IOException {
+    public Set<Class<?>> getUsedClasses() throws IOException, ClassNotFoundException {
         InputStream byteIn = clazz.getResourceAsStream(
                 clazz.getTypeName().replace(clazz.getPackageName() + ".", "") + ".class");
         var cr = new ClassReader(byteIn);
         cr.accept(this, 0);
+        if (!exceptions.isEmpty())
+            throw exceptions.get(0);
         return classesKnown;
     }
 
@@ -50,12 +54,19 @@ public class ClassIntrospection extends ClassVisitor {
 
     @Override
     public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-        return new MethodIntrospection();
+        try {
+            usedClasses.addAll(allClassNames(descriptor));
+        } catch (ClassNotFoundException e) {
+            this.exceptions.add(e);
+        }
+        return methodIntrospection;
     }
 
     private final static Pattern pattern = Pattern.compile("L(.*?)[;<]");
-    private static Set<Class<?>> allClassNames(String str) throws ClassNotFoundException {
+    static Set<Class<?>> allClassNames(String str) throws ClassNotFoundException {
         var result = new HashSet<Class<?>>();
+        if(str == null)
+            return result;
         Matcher matcher = pattern.matcher(str);
         while (matcher.find()) {
             result.add(Class.forName(matcher.group(1).replace("/", ".")));
@@ -65,6 +76,9 @@ public class ClassIntrospection extends ClassVisitor {
 
     @Override
     public void visitEnd() {
+        exceptions.addAll(methodIntrospection.getExceptions());
+
+        usedClasses.addAll(methodIntrospection.getUsedClasses());
         usedClasses.removeAll(classesKnown);
         classesKnown.addAll(usedClasses);
         usedClasses.forEach(aClass -> {
@@ -76,13 +90,10 @@ public class ClassIntrospection extends ClassVisitor {
             }
         });
 
-        classesKnown.addAll(recursiveUsed);
         super.visitEnd();
     }
 
-
-
-    public List<Exception> getExceptions() {
+    List<ClassNotFoundException> getExceptions() {
         return exceptions;
     }
 }
