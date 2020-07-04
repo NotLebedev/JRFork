3afd20e3f5eb68ac8d905400c710d55f4b5df5a5
NotLebedev <artem-itf@yandex.ru>
2020-07-04 22:36:50 +0300

    *Changed slave logic

--- example/Slave/src/org/slave/Main.java
+++ example/Slave/src/org/slave/Main.java
@@ -21,22 +21,40 @@ public class Main {
         Thread.currentThread().setContextClassLoader(cll);
 
         Instrumentation inst = InstrumentationHook.getInstrumentation();
+        SlaveConnection connection;
         try {
-            SlaveConnection connection = new SocketSlaveConnection(4040);
-            AbstractMessage message = connection.listenRequest();
-            if(message instanceof GetExecutionContextMessage) {
-                List<String> context = new ArrayList<>((new ExecutionContext(inst)).getExtraLoadedClassNames());
-                var response = new SendExecutionContextMessage(context);
-                connection.sendResponse(response);
-                message = connection.listenRequest();
-                if(message instanceof LoadClassesMessage) {
-                    ((LoadClassesMessage) message).getClassBytecodes().forEach((str,bytes) -> System.out.println(str));
-                    ((LoadClassesMessage) message).getClassBytecodes().forEach(cll::addClass);
-                }
-                connection.sendResponse(new ConnectionEstablishedMessage());
-            }
+            connection = new SocketSlaveConnection(4040);
         } catch (IOException e) {
             e.printStackTrace();
+            return;
+        }
+
+        while (true) {
+            AbstractMessage message;
+            try {
+                message = connection.listenRequest();
+            } catch (IOException e) {
+                break;
+            }
+            if (message instanceof GetExecutionContextMessage) {
+                List<String> context = new ArrayList<>((new ExecutionContext(inst)).getExtraLoadedClassNames());
+                var response = new SendExecutionContextMessage(context);
+                try {
+                    connection.sendResponse(response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            } else if (message instanceof LoadClassesMessage) {
+                ((LoadClassesMessage) message).getClassBytecodes().forEach((str, bytes) -> System.out.println(str));
+                ((LoadClassesMessage) message).getClassBytecodes().forEach(cll::addClass);
+                try {
+                    connection.sendResponse(new OperationSuccessfulMessage());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
         }
     }
 
