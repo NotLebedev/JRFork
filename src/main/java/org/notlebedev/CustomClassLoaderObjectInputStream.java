80ade63dc569272e27cd285bc21d42ae45a7ab7a
NotLebedev <artem-itf@yandex.ru>
2020-07-06 14:17:05 +0300

    +Description for CustomClassLoaderObjectInputStream

--- src/main/java/org/notlebedev/CustomClassLoaderObjectInputStream.java
+++ src/main/java/org/notlebedev/CustomClassLoaderObjectInputStream.java
@@ -2,6 +2,12 @@ package org.notlebedev;
 
 import java.io.*;
 
+/**
+ * CustomClassLoaderObjectInputStream deserializes primitive types and objects
+ * previously written by {@link ObjectOutputStream} with the same behaviour as
+ * {@link ObjectInputStream}, except it uses class loader specified instead of
+ * system class loader
+ */
 public class CustomClassLoaderObjectInputStream extends ObjectInputStream {
 
     private final ClassLoader classLoader;
