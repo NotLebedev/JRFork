9947e5a32a250c4b7a952c3b4655071659a81dfd
NotLebedev <artem-itf@yandex.ru>
2020-07-07 18:27:05 +0300

    +Documentation for Remote interface

--- src/main/java/org/notlebedev/Remote.java
+++ src/main/java/org/notlebedev/Remote.java
@@ -2,5 +2,9 @@ package org.notlebedev;
 
 import java.io.Serializable;
 
+/**
+ * Runnable + Serializable interface for remote execution of objects via
+ * {@link RemoteThread}
+ */
 public interface Remote extends Runnable, Serializable {
 }
