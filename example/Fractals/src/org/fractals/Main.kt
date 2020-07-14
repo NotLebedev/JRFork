471fc835b8bed567e849e863ee2b62bc970d9a38
NotLebedev <artem-itf@yandex.ru>
2020-07-14 18:23:41 +0300

    *Converted to use RemoteThread

--- example/Fractals/src/org/fractals/Main.kt
+++ example/Fractals/src/org/fractals/Main.kt
@@ -1,32 +1,56 @@
 package org.fractals
 
+import org.notlebedev.Remote
+import org.notlebedev.RemoteThread
+import org.notlebedev.introspection.ObjectIntrospection
+import org.notlebedev.networking.MasterConnection
+import org.notlebedev.networking.SocketMasterConnection
 import java.awt.image.BufferedImage
 import java.io.File
+import java.net.InetAddress
 import javax.imageio.ImageIO
 
 
 fun main() {
     val fileOut = File("img.png")
-    ImageIO.write(Mandelbrot().image, "png", fileOut)
+    val mandelbrot = Mandelbrot()
+
+    val connection: MasterConnection = SocketMasterConnection(InetAddress.getLocalHost(), 4040, 8081)
+    val remoteThread = RemoteThread(connection, mandelbrot)
+
+    remoteThread.start()
+    remoteThread.setInaccessibleModulePolicy(ObjectIntrospection.InaccessibleModulePolicy.WARN)
+
+    println("Thread is running remotely")
+
+    remoteThread.join()
+    if (!remoteThread.isSuccessful) {
+        remoteThread.exception.printStackTrace()
+        return
+    }
+
+    mandelbrot.image?.let { ImageIO.write(it, "png", fileOut) }
+            ?: throw IllegalStateException("Expected to be not null")
+
 }
 
-class Mandelbrot {
+class Mandelbrot : Remote {
     private val maximumIterations = 512
     private val width = 800
     private val height = 800
-    private val img: BufferedImage
+    private var img: BufferedImage? = null
 
     private val x0: Double = -1.88488933694469 - 0.00000000000024
     private val y0: Double = 0.00000000081387 - 0.00000000000024
     private val x1: Double = -1.88488933694469 + 0.00000000000024
     private val y1: Double = 0.00000000081387 + 0.00000000000024
 
-    val image: BufferedImage
+    val image: BufferedImage?
         get() {
             return img
         }
 
-    init {
+    override fun run() {
         val xStep:Double = (x1 - x0) / width
         val yStep:Double = (y1 - y0) / height
 
@@ -43,7 +67,7 @@ class Mandelbrot {
                             .also { zx = zx * zx - zy * zy + cX }
                     iter--
                 }
-                img.setRGB(x, y, iter or (iter shl 8))
+                img!!.setRGB(x, y, iter or (iter shl 8))
             }
         }
     }
