4ca91cc555a6b1920783d5f3d20753e3a145d3bf
NotLebedev <artem-itf@yandex.ru>
2020-07-15 12:47:54 +0300

    +Option to set port for slave

--- example/Slave/src/org/slave/Main.java
+++ example/Slave/src/org/slave/Main.java
@@ -5,13 +5,18 @@ import org.notlebedev.networking.SlaveConnection;
 import org.notlebedev.networking.SocketSlaveConnection;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        int port = 4040;
+        if(args.length > 0 && args[0].equals("--port"))
+            port = new Scanner(args[1]).nextInt();
+
         SlaveConnection connection;
         try {
-            connection = new SocketSlaveConnection(4040);
+            connection = new SocketSlaveConnection(port);
         } catch (IOException e) {
             e.printStackTrace();
             return;
